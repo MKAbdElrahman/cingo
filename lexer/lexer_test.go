0e7d96e7e49fe2bc2c7388b80d156652161635d0
mkabdelrahman <mkcodedev@gmail.com>
2024-03-31 23:50:57 +0200

    lex braces and parenthesis

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -23,6 +23,27 @@ func TestLexSemicolon(t *testing.T) {
 	}
 }
 
+func TestLexbrace(t *testing.T) {
+
+	var regexMap = map[string]*regexp.Regexp{
+		"Semicolon":         regexp.MustCompile(`^;`),
+		"Openbrace":         regexp.MustCompile(`^{`),
+		"Closedbrace":       regexp.MustCompile(`^}`),
+		"Openparenthesis":   regexp.MustCompile(`^\(`),
+		"Closedparenthesis": regexp.MustCompile(`^\)`),
+	}
+
+	input := " { ; } ()"
+	expectedTokens := []string{"{", ";", "}","(",")"}
+	tokens, err := lexer.Lex(input, regexMap)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+	if !compareSlices(tokens, expectedTokens) {
+		t.Errorf("Expected tokens %v, got %v", expectedTokens, tokens)
+	}
+}
+
 func compareSlices(a, b []string) bool {
 	if len(a) != len(b) {
 		return false
